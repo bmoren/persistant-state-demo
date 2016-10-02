c75991da53a357bbb61183d51cc5056541b3efc7
bmoren <ben@benmoren.com>
2016-10-02 18:39:42 -0500

    'positon, objects, storing info

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var app = require('express')() // use the express js framework to server out html files (handle the request and response on the server)
+// var app = require('express')() // use the express js framework to server out html files (handle the request and response on the server)
+var express = require('express');
+var app = express();
 var server = require('http').Server( app ) // start a server instance on a port
 var io = require('socket.io')(server) // use socket.io for real time connections aka. wesockets
 
@@ -6,21 +8,19 @@ server.listen(3000, function(){ // set up a server on port 3000, do a callback w
   console.log("server started on 3000");
 })
 
-app.get('/', function(req, res){ // look at the root request using express.js (notice the 'app' at the beginning)
-  res.sendFile(__dirname + '/public/index.html') //after a request, respond aka. 'res' with the file that we want to send back to the user, in this case index.html
-})
+app.use(express.static('public')); //load out anything in the 'public' folder
 
-app.get('/projection', function(req, res){ // look at the /projection path and send back the projection.html file
-  res.sendFile(__dirname + '/public/projection.html')
-
-})
+var storedPositions = [];
 
 io.on('connection', function(socket){  // if socket.io sees a new connection, do something with them...
   console.log(socket.id) // prints out the socket that connected (ie: all users + the projection)
 
+  io.emit('storedPositions', storedPositions); // when someone connects, send out the stored positions to them...
+
   socket.on('addRectangle', function(data){ //look for any messages with the 'addRectangle'
     console.log("addRectangle" + data); // log out the 'data' in this case you get true, but you could use this to get and arbitray data you want, think position, color, etc.
-    io.emit('projectionRectangle', true); // send out a message to the projection to add a rect to the screen.
+    storedPositions.push(data) //add the stored positions into the array for temp storage as long as the server is up.
+    io.emit('projectionRectangle', data); // send out a message to the projection to add a rect to the screen.
 
   })
 
---
+++ public/projection.js
@@ -0,0 +1,34 @@
+var socket = io.connect('http://localhost:3000') //set up a place for us to connect to, and try to connect.
+
+socket.on('connect', function(data){ // when we are connected do something.
+  console.log("connected to the server" + socket.id); // log out our socket's id, some long garbled number letter thing that is unique!
+})
+
+socket.on('storedPositions', function(data){
+  console.log(data)
+
+  data.forEach(function(position){
+    $('<div>👻</div>').css({
+      'position':'absolute',
+      'top' : position.top,
+      'left' : position.left,
+
+    }).appendTo('body')
+
+
+  })
+})
+
+socket.on('projectionRectangle', function(data){ // if we see a projectionRectangle message then do some stuff
+  console.log(data) //see the coordinates come down.
+
+
+    //first make a div, chage the css, then append to the body. this is not possible with the 'normal' syntax, becayse order of operations (with chained methods)
+    $('<div>🔥</div>').css({
+      'position':'absolute',
+      'top' : data.top,
+      'left' : data.left,
+
+    }).appendTo('body')
+
+})
